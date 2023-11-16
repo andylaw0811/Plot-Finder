c95396f5853dd9a08c9ae4365e01f309dfeef0df
andylaw0811 <78982705+andylaw0811@users.noreply.github.com>
2023-11-16 00:17:44 +0000

    Update Folium.py

--- Folium.py
+++ Folium.py
@@ -80,7 +80,7 @@ if authentication_status:
             building_bool = pd.DataFrame(row).notnull()
             for (k, v), bool in zip(row.items(), building_bool):
                 if k != "geometry" and building_bool[bool][0]:
-                    popup_text += "<b>{}: </b>{} <br>".format(k, v)
+                    popup_text = popup_text + "<b>{}: </b>{} <br>".format(k, v)
             folium.Marker(
                 location=[lat, lon],
                 popup=popup_text,
